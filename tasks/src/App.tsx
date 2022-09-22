a4575549c7c4929b0f2c22a6da5deeba67090cbe
semionava <semionavanesyan@gmail.com>
2022-09-22 12:34:43 +0300

    added number and reset button

--- tasks/src/App.tsx
+++ tasks/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Youtube} from "./components/Youtube";
 // import {NewComponent} from "./components/NewComponent";
@@ -13,10 +13,25 @@ import {Youtube} from "./components/Youtube";
 
 
 function App() {
+    let [a, setA] = useState(1)
+
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
+    }
+
+    const onClickHandler2=()=>{
+        setA(a=0)
+        console.log(a)
+    }
+
     return (
         <div className="App">
             {/*<NewComponent topCars={cars}/>*/}
             {/*<Youtube/>*/}
+            {/*<h1>{a}</h1>*/}
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>reset</button>
         </div>
     );
 }
